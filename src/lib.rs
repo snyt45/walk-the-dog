8fea34f4817267d62c98f1a12dc0bfa0d728aa7b
yuta.sano <snyt45@gmail.com>
2023-10-09 13:13:55 +0000

    シェルピンスキーの三角形を削除

--- src/lib.rs
+++ src/lib.rs
@@ -25,7 +25,6 @@ struct Sheet {
     frames: HashMap<String, Cell>,
 }
 
-// This is like the `main` function, except for JavaScript.
 #[wasm_bindgen(start)]
 pub fn main_js() -> Result<(), JsValue> {
     console_error_panic_hook::set_once();
@@ -69,13 +68,6 @@ pub fn main_js() -> Result<(), JsValue> {
 
         success_rx.await;
         context.draw_image_with_html_image_element(&image, 0.0, 0.0);
-
-        sierpinski(
-            &context,
-            [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)],
-            (0, 255, 0),
-            5,
-        );
 
         let json = fetch_json("rhb.json")
             .await
@@ -128,65 +120,4 @@ async fn fetch_json(json_path: &str) -> Result<JsValue, JsValue> {
     let resp: web_sys::Response = resp_value.dyn_into()?;
 
     wasm_bindgen_futures::JsFuture::from(resp.json()?).await
-}
-
-fn draw_triangle(
-    context: &web_sys::CanvasRenderingContext2d,
-    points: [(f64, f64); 3],
-    color: (u8, u8, u8),
-) {
-    let color_str = format!("rgb({}, {}, {})", color.0, color.1, color.2);
-    context.set_fill_style(&wasm_bindgen::JsValue::from_str(&color_str));
-    let [top, left, right] = points;
-    context.move_to(top.0, top.1);
-    context.begin_path();
-    context.line_to(left.0, left.1);
-    context.line_to(right.0, right.1);
-    context.line_to(top.0, top.1);
-    context.close_path();
-    context.stroke();
-    context.fill();
-}
-
-fn sierpinski(
-    context: &web_sys::CanvasRenderingContext2d,
-    points: [(f64, f64); 3],
-    color: (u8, u8, u8),
-    depth: u8,
-) {
-    draw_triangle(&context, points, color);
-    let depth = depth - 1;
-
-    let [top, left, right] = points;
-    // 再帰処理の終了条件
-    if depth > 0 {
-        let mut rng = thread_rng();
-        let next_color = (
-            rng.gen_range(0..255),
-            rng.gen_range(0..255),
-            rng.gen_range(0..255),
-        );
-
-        let left_middle = ((top.0 + left.0) / 2.0, (top.1 + left.1) / 2.0);
-        let right_middle = ((top.0 + right.0) / 2.0, (top.1 + right.1) / 2.0);
-        let bottom_middle = (top.0, right.1);
-        sierpinski(
-            &context,
-            [top, left_middle, right_middle],
-            next_color,
-            depth,
-        );
-        sierpinski(
-            &context,
-            [left_middle, left, bottom_middle],
-            next_color,
-            depth,
-        );
-        sierpinski(
-            &context,
-            [right_middle, bottom_middle, right],
-            next_color,
-            depth,
-        );
-    }
 }
