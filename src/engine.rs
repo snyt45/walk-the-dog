ce3b02ff02d4183793777b2a7c459e59a074da6c
yuta.sano <snyt45@gmail.com>
2023-10-20 12:44:11 +0000

    描画コードの整理

--- src/engine.rs
+++ src/engine.rs
@@ -1,13 +1,54 @@
 use crate::browser::{self, LoopClosure};
 use anyhow::{anyhow, Result};
+use async_trait::async_trait;
 use futures::channel::oneshot::channel;
 use std::{cell::RefCell, rc::Rc, sync::Mutex};
 use wasm_bindgen::{prelude::Closure, JsCast, JsValue};
 use web_sys::{CanvasRenderingContext2d, HtmlImageElement};
 
+pub struct Rect {
+    pub x: f32,
+    pub y: f32,
+    pub width: f32,
+    pub height: f32,
+}
+
+pub struct Renderer {
+    context: CanvasRenderingContext2d,
+}
+
+impl Renderer {
+    pub fn clear(&self, rect: &Rect) {
+        self.context.clear_rect(
+            rect.x.into(),
+            rect.y.into(),
+            rect.width.into(),
+            rect.height.into(),
+        )
+    }
+
+    pub fn draw_image(&self, image: &HtmlImageElement, frame: &Rect, destination: &Rect) {
+        self.context
+            .draw_image_with_html_image_element_and_sw_and_sh_and_dx_and_dy_and_dw_and_dh(
+                &image,
+                frame.x.into(),
+                frame.y.into(),
+                frame.width.into(),
+                frame.height.into(),
+                destination.x.into(),
+                destination.y.into(),
+                destination.width.into(),
+                destination.height.into(),
+            )
+            .expect("Drawing is throwing exceptions! Unrecoverable error.");
+    }
+}
+
+#[async_trait(?Send)]
 pub trait Game {
+    async fn initialize(&self) -> Result<Box<dyn Game>>;
     fn update(&mut self);
-    fn draw(&self, context: &CanvasRenderingContext2d);
+    fn draw(&self, renderer: &Renderer);
 }
 
 const FRAME_SIZE: f32 = 1.0 / 60.0 * 1000.0;
@@ -20,9 +61,14 @@ type SharedLoopClosure = Rc<RefCell<Option<LoopClosure>>>;
 
 impl GameLoop {
     pub async fn start(mut game: impl Game + 'static) -> Result<()> {
+        let mut game = game.initialize().await?;
         let mut game_loop = GameLoop {
             last_frame: browser::now()?,
             accumulated_delta: 0.0,
+        };
+
+        let renderer = Renderer {
+            context: browser::context()?,
         };
         let f: SharedLoopClosure = Rc::new(RefCell::new(None));
         let g = f.clone();
@@ -34,7 +80,7 @@ impl GameLoop {
                 game_loop.accumulated_delta -= FRAME_SIZE;
             }
             game_loop.last_frame = perf;
-            game.draw(&browser::context().expect("Context should exist"));
+            game.draw(&renderer);
             browser::request_animation_frame(f.borrow().as_ref().unwrap());
         }));
 
